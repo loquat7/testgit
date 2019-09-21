ea69bf7d8a81216c3eeb3f9367ce24c30df84b42
loquat7 <loquat7@gmail.com>
2019-09-21 18:30:03 +0800

    test again

--- src/testgit/Hello.java
+++ src/testgit/Hello.java
@@ -3,7 +3,7 @@ package testgit;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello GitHub ... ");
+		System.out.println("Hello GitHub ... 123456789");
 
 	}
 
